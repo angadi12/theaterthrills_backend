0149e122d93e2e4f1e517c951bb10d19d31f03cc
angadi12 <santosh.alimkar@outlook.com>
2024-12-02 23:53:40 +0530

    Update Connection.js

--- Services/Db/Connection.js
+++ Services/Db/Connection.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const DbConnection = () => {
   try {
     mongoose
-      .connect(process.env.MONGOURL)
+      .connect("mongodb+srv://Developer:DevTeam%402024@theater-thrills.rikcm.mongodb.net/THEATERTHRILLS")
       .then((res) => {
         console.log("DATABASE CONNECTED SUCCESSFULLY");
       })
